1285ac5b0004c7027f10f8c753e5021a6d61b0f2
Daniel Thorn <dthorn@mozilla.com>
2017-05-18 09:23:38 -0700

    add file pointer refreshing to file_buffer

--- kcl_connectors/file_buffer.py
+++ kcl_connectors/file_buffer.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 from amazon_kclpy.kcl import KCLProcess
+from datetime import datetime
 from kcl_connectors import base
 from os import getenv
 
 class RecordProcessor(base.RecordProcessor):
     # file where logs are written
     file_buffer = open(getenv('FILE_BUFFER'), 'a')
+    file_buffer_opened = datetime.now()
+    file_buffer_max_age = int(getenv('FILE_BUFFER_MAX_AGE', 60))
 
     def process_record(self, data, partition_key, sequence_number):
+        if (datetime.now() - self.file_buffer_opened).seconds > self.file_buffer_max_age:
+            self.file_buffer.close()
+            self.file_buffer = open(getenv('FILE_BUFFER'), 'a')
+            self.file_buffer_opened = datetime.now()
         self.file_buffer.write(data)
         self.file_buffer.flush()
 
