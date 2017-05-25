6d8581a847f83baf0cef8e57a8693afdc649b2bf
Daniel Thorn <dthorn@mozilla.com>
2017-05-25 09:54:22 -0700

    modify file connector to check for file rotation with stat on each write

--- kcl_connectors/file.py
+++ kcl_connectors/file.py
@@ -3,20 +3,21 @@ from amazon_kclpy.kcl import KCLProcess
 from datetime import datetime
 from kcl_connectors import base
 from os import getenv
+from os import fstat
+from os import stat
 
 class RecordProcessor(base.RecordProcessor):
     # file where logs are written
-    fp = open(getenv('FILE'), 'a')
-    # when self.fp was opened
-    fp_opened = datetime.now()
-    # seconds we write to self.fp before reopening it
-    fp_max_age = int(getenv('FILE_MAX_AGE', 60))
+    fname = getenv('FILE')
+    fp = None
+    fp_ino = None
 
     def process_record(self, data, partition_key, sequence_number):
-        if (datetime.now() - self.fp_opened).seconds > self.fp_max_age:
-            self.fp.close()
-            self.fp = open(getenv('FILE'), 'a')
-            self.fp_opened = datetime.now()
+        if self.fp_ino != stat(self.fname).st_ino
+            if self.fp is not None:
+                self.fp.close()
+            self.fp = open(self.fname, 'a')
+            self.fp_ino = fstat(self.fp.fileno()).st_ino
         self.fp.write(data + "\n")
         self.fp.flush()
 
